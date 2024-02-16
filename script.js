8075d2edb0218591978007b0f9e98856778d3857
giovanifrota <giovanifrota45@gmail.com>
2024-02-16 10:44:11 -0300

    ADICIONANDO ENUM!

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ const pontosAdversario = document.querySelector("#pontos-maquina")
 let pontosHumano = 0
 let pontosMaquina = 0
 
+const GAME_OPTION = {
+    PEDRA: 'rock',
+    PAPEL: 'paper',
+    TESOURA: 'scissors'
+}
+
 const playHumano = (escolhaHumano) =>{
     jogarJogo(escolhaHumano, playMaquina())
 }
 
 const playMaquina = () =>{
-    const escolhas = ['rock', 'paper', 'scissors']
+    const escolhas = [GAME_OPTION.PEDRA, GAME_OPTION.PAPEL, GAME_OPTION.TESOURA]
 
     const numerosAleatorios = Math.floor(Math.random()*3)
 
@@ -24,7 +30,7 @@ const jogarJogo = (humano, maquina) =>{
     console.log('Humano :' + humano + ' Maquina :' + maquina)
     if(humano === maquina){
         resultado.innerHTML = 'Deu empate'
-    } else if((humano === 'paper' && maquina === 'rock')|| (humano === 'rock' && maquina === 'scissors') || (humano === 'scissors' && maquina === 'paper')){
+    } else if((humano === GAME_OPTION.PAPEL && maquina === GAME_OPTION.PEDRA)|| (humano === GAME_OPTION.PEDRA && maquina === GAME_OPTION.TESOURA) || (humano === GAME_OPTION.TESOURA && maquina === GAME_OPTION.PAPEL)){
         pontosHumano++
         meusPontos.innerHTML = pontosHumano 
         resultado.innerHTML = 'Você ganhou'
